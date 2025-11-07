c083bccb64cee5bb41b0f71d968fa16b2fc3b139
vivalafit <navrotskymax@gmail.com>
2025-11-07 08:01:59 +0200

    tests

--- src/sum.js
+++ src/sum.js
@@ -2,7 +2,7 @@ function sum(a, b) {
   if (typeof a !== "number" || typeof b !== "number") {
     throw new TypeError("nums only");
   }
-  console.log("test log");
+  console.log("test log 2");
   return a + b;
 }
 
